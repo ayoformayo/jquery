9c444f90f4a70d8b0b05b22994a97d22b14c6d3d
dbc-apprentice <apprentice@devbootcamp.com>
2013-07-03 14:03:29 -0500

    first draft

--- public/js/application.js
+++ public/js/application.js
@@ -1,3 +1,17 @@
-$(document).ready(function(){
+function onReady() {
+  $('#get_color').submit(onSubmit);
+}
 
-});
+function onSubmit(event){
+  event.preventDefault();
+  var serialized = $('#get_color').serialized;
+  $.post('/color', serialized, onSuccess);
+}
+
+function onSuccess(success){
+
+  alert(color)
+
+}
+
+$(document).ready(onReady);
